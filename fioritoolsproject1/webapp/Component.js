fb4765bd3973b56d1962cd76faaf58b84d5cec54
James Rosales Caballero <james.virgo30@gmail.com>
2024-05-25 12:39:22 -0500

    first commit

---
+++ fioritoolsproject1/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.jrosales.products.fioritoolsproject1.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
